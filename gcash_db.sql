969d3d598520f29380010d35cedf653b208c12d9
Pekong Pogi <jasperkarlpatio0921@gmail.com>
2025-06-02 03:24:04 +0800

    Add files via upload

---
+++ gcash_db.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 01, 2025 at 08:51 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gcash_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_record`
+--
+
+CREATE TABLE `tbl_record` (
+  `id` int(50) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `refer` bigint(200) NOT NULL,
+  `no` bigint(50) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `amount` int(50) NOT NULL,
+  `charge` int(50) NOT NULL,
+  `type` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_record`
+--
+
+INSERT INTO `tbl_record` (`id`, `name`, `refer`, `no`, `address`, `date`, `amount`, `charge`, `type`) VALUES
+(1, 'jasper', 3263232323, 9628463726, 'Casiguran', '2025-05-28 17:07:09', 1000, 10, 'cashin');;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `username`, `password`) VALUES
+(1, 'pekong', '1234'),
+(2, 'jasper', '1234'),
+(3, 'jmie', '4567'),
+(4, 'jmie', '1234'),
+(5, 'jmie', '56789'),
+(6, 'Jasper Karl', '12345');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_record`
+--
+ALTER TABLE `tbl_record`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_record`
+--
+ALTER TABLE `tbl_record`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
